a2998cb3890d4df365d014be8afb3425e68ebc3a
Tomachi[Main] <lego@aol.jp>
2019-01-24 15:15:21 +0900

    first commit

--- MediaConverter/Form1.Designer.cs
+++ MediaConverter/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(483, -1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(485, 455);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 15F);
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 120);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. 形式を選択\r\n\r\n\r\n2. ここに変換するファイルをドラックアンドドロップ\r\n3. 「Run」をクリック\r\n4. 処理完了ポップアップが表示されたら完了";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mp3",
+            "m4a"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(410, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 415);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(484, 39);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 179);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.button1.Location = new System.Drawing.Point(12, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.button2.Location = new System.Drawing.Point(374, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(967, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MediaConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MediaConverter/Form1.cs
+++ MediaConverter/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.ListBox;
 
 namespace MediaConverter
 {
@@ -15,6 +18,112 @@ namespace MediaConverter
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addLogBox("Application Start");
+
+            comboBox1.SelectedIndex = 0;
+
+            if (!File.Exists("ffmpeg.exe"))
+            {
+                MessageBox.Show("同じディレクトリ内にffmpeg.exeを配置してください。",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            if (!Directory.Exists("output"))
+            {
+                Directory.CreateDirectory("output");
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            int i = 0;
+            foreach (string file in files)
+            {
+                if(!listBox1.Items.Contains(file)){
+                    listBox1.Items.Add(file);
+                    i++;
+                }
+            }
+            addLogBox("Added " + i);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            addLogBox("Cleared List");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            addLogBox("Process Start");
+
+            addLogBox("Files Count: " + listBox1.Items.Count);
+
+            int success = 0;
+            int error = 0;
+
+            List<string> removes = new List<string>();
+
+            progressBar1.Maximum = listBox1.Items.Count;
+            progressBar1.Value = 0;
+
+            foreach (string path in listBox1.Items)
+            {
+                string filePath = @"output\" + Path.GetFileNameWithoutExtension(path) + "." + comboBox1.SelectedItem;
+                addLogBox(Path.GetFileNameWithoutExtension(path));
+                addLogBox(Path.GetExtension(path) + " ->" + comboBox1.SelectedItem);
+
+                Process p = Process.Start(
+                    "ffmpeg.exe",
+                    "-y -i \"" + path + "\" \"" + filePath + "\"");
+
+                p.WaitForExit();
+                removes.Add(path);
+                
+                if(p.ExitCode == 0)
+                {
+                    addLogBox("Success!");
+                    success++;
+                }
+                else
+                {
+                    addLogBox("Error...");
+                    error++;
+                }
+                progressBar1.Value++;
+            }
+            foreach(string remove in removes)
+            {
+                listBox1.Items.Remove(remove);
+            }
+            
+            MessageBox.Show("処理終了" + Environment.NewLine + "成功数: " + success + Environment.NewLine + "失敗数: " + error,
+                    "Result",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            addLogBox("成功数: " + success);
+            addLogBox("失敗数: " + error);
+            button2.Enabled = true;
+        }
+
+        void addLogBox(string text)
+        {
+            string date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            textBox1.Text = textBox1.Text + "[" + date + "] " + text + Environment.NewLine;
         }
     }
 }
